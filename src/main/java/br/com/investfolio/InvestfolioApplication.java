4e207cdbbb2131283125c5767eb4bd90231c0fe9
vitor Alexandre Moschetti <vitor.alexandre.moschetti@everis.nttdata.com>
2022-10-18 19:06:07 -0300

    first commit

---
+++ src/main/java/br/com/investfolio/InvestfolioApplication.java
@@ -0,0 +1,13 @@
+package br.com.investfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestfolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/investfolio/InvestfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.investfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
